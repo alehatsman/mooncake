14ce494f5adc8b74208734d475f2fe9cb26ae093
Aleh Atsman <aleh.atsman@gmail.com>
2023-03-07 20:35:23 +0800

    win32yank

--- mooncake-automation/nvim/templates/init.lua
+++ mooncake-automation/nvim/templates/init.lua
@@ -732,3 +732,18 @@ for _, language in ipairs({ "typescript", "javascript" }) do
     }
   }
 end
+
+{% if os == "windows" && use_win32yank %}
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf"
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf"
+  },
+  cache_enabled = 0
+}
+{% endif %}
